63dfd263c84a378175b27dab828f0f117e963611
Filz0r <filipefig26@gmail.com>
2023-08-22 17:03:58 +0100

    updated initalizing function and removed unneeded functions

--- push_swap/utils/list_utils.c
+++ push_swap/utils/list_utils.c
@@ -6,7 +6,7 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 15:19:37 by fparreir          #+#    #+#             */
-/*   Updated: 2023/08/22 10:52:09 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/08/22 16:49:49 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	add_list_end(t_plist **start, int num)
 	if (new_node == NULL)
 		errors();
 	new_node->data = num;
-	new_node->pos = -1;
+	new_node->info = NULL;
 	new_node->next = NULL;
 	if (*start == NULL)
 	{
@@ -51,20 +51,3 @@ size_t	list_length(t_plist **start)
 	return (i);
 }
 
-void	set_list_positions(t_plist **start, int *array, int array_size)
-{
-	t_plist	*curr;
-	int		index;
-
-	if (*start == NULL)
-		return ;
-	curr = *start;
-	while (curr)
-	{
-		index = get_array_index(array, array_size, curr->data);
-		if (index < 0)
-			errors();
-		curr->pos = index;
-		curr = curr->next;
-	}
-}
