fe61b0d014c14c56b6dc4ed0c2e19e9ad15d299c
Filz0r <filipefig26@gmail.com>
2023-08-22 17:07:11 +0100

    this does nothing yet

--- push_swap/sort_three.c
+++ push_swap/sort_three.c
@@ -6,24 +6,24 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 19:26:20 by fparreir          #+#    #+#             */
-/*   Updated: 2023/08/22 08:07:34 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/08/22 11:19:47 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/push_swap.h"
 
 
-void	sort_three(t_plist **stack_a, t_plist **stack_b, int *array, int size)
+void	sort_three(t_plist **stack_a, t_plist **stack_b)
 {
-	int			current_position;
-	t_plist		*curr;
+	t_plist	*curr;
 
 	if (*stack_a == NULL || *stack_b != NULL)
 		errors();
 	curr = *stack_a;
 	while (curr)
 	{
-		current_position = get_current_position();
+
 		curr = curr->next;
 	}
+
 }
