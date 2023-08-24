b6f1533507c4c9fc15c2f2b395119c5005b1b3c0
Filz0r <filipefig26@gmail.com>
2023-08-24 13:51:24 +0100

    starting to lose my mind

--- push_swap/sort_five.c
+++ push_swap/sort_five.c
@@ -6,14 +6,30 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 15:56:40 by fparreir          #+#    #+#             */
-/*   Updated: 2023/08/23 15:56:40 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/08/24 11:33:26 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/push_swap.h"
 
-void	sort_five(t_plist **root_a, t_plist **root_b)
+static void	rule_handle(t_plist **a, t_plist **b)
 {
-	if (*root_b == NULL || *root_a == NULL)
+
+}
+
+static void	handle_four(t_plist **stack_a, t_plist **stack_b)
+{
+	pb(stack_a, stack_b, 1);
+	sort_three(stack_a, 3);
+	rule_handle(stack_a, stack_b);
+	print_list_long(stack_a);
+	print_list_long(stack_b);
+}
+
+void	sort_five(t_plist **root_a, t_plist **root_b, int size)
+{
+	if (is_sorted(root_a))
 		return ;
+	if (size == 4)
+		return (handle_four(root_a, root_b));
 }
