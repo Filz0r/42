ae4b6decec7a94be98ece26ba80dbaa5ce924db9
Filz0r <filipefig26@gmail.com>
2023-08-24 13:50:56 +0100

    refactored this file to sort with comparison instead of location, prev logic caused issues in sort_five

--- push_swap/sort_three.c
+++ push_swap/sort_three.c
@@ -6,12 +6,13 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 19:26:20 by fparreir          #+#    #+#             */
-/*   Updated: 2023/08/23 17:16:28 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/08/24 13:21:07 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/push_swap.h"
 
+// else handles this case (first->data > second->data && second->data > third->data)
 static int	case_check(t_plist **start)
 {
 	t_plist	*first;
@@ -21,17 +22,15 @@ static int	case_check(t_plist **start)
 	first = *start;
 	second = (*start)->next;
 	third = second->next;
-	if (first->info->curr_pos == first->info->final_pos
-		&& second->info->curr_pos == (second->info->final_pos - 1))
+	if (first->data < second->data && second->data > third->data
+		&& first->data < third->data)
 		return (1);
-	else if (first->info->final_pos == (first->info->stack_size - 1)
-		&& second->info->final_pos == second->info->curr_pos)
+	else if (first->data > second->data && second->data < third->data
+		&& first->data < third->data)
 		return (2);
-	else if ((first->info->curr_pos + 1) == first->info->final_pos
-		&& (second->info->curr_pos + 1) == second->info->final_pos)
+	else if (first->data < second->data && second->data > third->data)
 		return (3);
-	else if (third->info->curr_pos == third->info->final_pos
-		&& first->data > second->data)
+	else if (first->data > second->data && second->data < third->data)
 		return (4);
 	else
 		return (5);
@@ -53,14 +52,14 @@ void	sort_three(t_plist **stack, int size)
 		sa(stack, 1);
 	}
 	else if (case_check(stack) == 2)
-	{
-		ra(stack, 1);
 		sa(stack, 1);
-	}
 	else if (case_check(stack) == 3)
 		rra(stack, 1);
 	else if (case_check(stack) == 4)
-		sa(stack, 1);
-	else if (case_check(stack) == 5)
 		ra(stack, 1);
+	else if (case_check(stack) == 5)
+	{
+		sa(stack, 1);
+		rra(stack, 1);
+	}
 }
