3716b1262d4f7dc19a020e6e84465060e52d6d06
Filz0r <filipefig26@gmail.com>
2023-08-25 16:53:11 +0100

    added function to free allocated memory

--- push_swap/utils/utils.c
+++ push_swap/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 13:53:25 by fparreir          #+#    #+#             */
-/*   Updated: 2023/08/23 18:11:50 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/08/25 16:20:40 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,23 @@ int	has_digits(const char *str)
 		return (0);
 	else
 		return (1);
+}
+
+void	free_list(t_plist **start)
+{
+	t_plist	*curr;
+	t_plist	*temp;
+
+	if (*start == NULL)
+		return ;
+	curr = *start;
+	while (curr)
+	{
+		temp = curr;
+		curr = curr->next;
+		free(temp->info);
+		free(temp);
+	}
 }
 
 void	errors(void)
