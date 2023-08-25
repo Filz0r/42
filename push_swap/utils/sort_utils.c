70b7cfa63749d589b22275626836840714ce6d16
Filz0r <filipefig26@gmail.com>
2023-08-25 16:52:49 +0100

    removed logic for counting moves and improved info update

--- push_swap/utils/sort_utils.c
+++ push_swap/utils/sort_utils.c
@@ -6,29 +6,57 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 10:54:02 by fparreir          #+#    #+#             */
-/*   Updated: 2023/08/23 13:22:43 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/08/25 16:10:20 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-void	initialize_move_counters(t_plist **start)
+int	get_largest(t_plist **start)
 {
 	t_plist	*curr;
+	int		res;
 
-	if (*start == NULL)
-		errors();
+	res = INT_MIN;
 	curr = *start;
 	while (curr)
 	{
-		curr->info->pa = -1;
-		curr->info->pb = -1;
-		curr->info->sa = -1;
-		curr->info->sb = -1;
-		curr->info->ra = -1;
-		curr->info->rb = -1;
-		curr->info->rra = -1;
-		curr->info->rrb = -1;
+		if (curr->data > res)
+			res = curr->data;
+		curr = curr->next;
+	}
+	return (res);
+}
+
+int	get_smallest(t_plist **start)
+{
+	t_plist	*curr;
+	int		res;
+
+	res = INT_MAX;
+	curr = *start;
+	while (curr)
+	{
+		if (curr->data < res)
+			res = curr->data;
+		curr = curr->next;
+	}
+	return (res);
+}
+
+void	set_smallest_and_biggest(t_plist **start)
+{
+	t_plist	*curr;
+	int		small;
+	int		big;
+
+	curr = *start;
+	small = get_smallest(start);
+	big = get_largest(start);
+	while (curr)
+	{
+		curr->info->smallest_in_stack = small;
+		curr->info->biggest_in_stack = big;
 		curr = curr->next;
 	}
 }
@@ -39,6 +67,7 @@ void	info_init(t_plist **start, int *array, int size)
 	t_info	*new;
 	int		i;
 
+
 	if (*start == NULL)
 		errors();
 	i = 0;
@@ -48,7 +77,6 @@ void	info_init(t_plist **start, int *array, int size)
 		new = malloc(sizeof(t_info));
 		if (!new)
 			errors();
-		new->parent = curr;
 		new->stack = 'a';
 		new->stack_size = size;
 		new->final_pos = get_array_index(array, size, curr->data);
@@ -57,7 +85,7 @@ void	info_init(t_plist **start, int *array, int size)
 		curr->info = new;
 		curr = curr->next;
 	}
-	initialize_move_counters(start);
+	set_smallest_and_biggest(start);
 }
 
 void	update_info(t_plist **start, char stack)
@@ -81,4 +109,5 @@ void	update_info(t_plist **start, char stack)
 		curr->info->stack_size = i;
 		curr = curr->next;
 	}
+	set_smallest_and_biggest(start);
 }
