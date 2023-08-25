a78b0a8269e0fee6e812779ab5193ebf834767c0
Filz0r <filipefig26@gmail.com>
2023-08-25 16:54:32 +0100

    almost finished with main file, just need to remove comments and add large sort

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 14:58:03 by fparreir          #+#    #+#             */
-/*   Updated: 2023/08/24 11:23:29 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/08/25 16:21:34 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,23 @@ int	main(int ac, char **av)
 		array = handle_args(--ac, av, &root_a);
 		bubble_sort(array, ac);
 		info_init(&root_a, array, ac);
-		ft_printf("before "); // remove later
-		print_list(&root_a); // remove later
+		//ft_printf("before "); // remove later
+		//print_list(&root_a); // remove later
 		free(array);
+		if (is_sorted(&root_a))
+		{
+			free_list(&root_a);
+			return (0);
+		}
 		if (ac <= 3)
 			sort_three(&root_a, ac);
 		else if (ac <= 5)
-			sort_five(&root_a, &root_b, ac);
+			sort_five(&root_a, &root_b);
 		//something like that freees the data
 		// the errors function should also call this function when something
 		// goes wrong with the program, to avoid leaks.
-		//free_list(&root_a);
-		//free_list(&root_b);
-		ft_printf("after "); // remove later
-		print_list_long(&root_a); // remove later
+		free_list(&root_a);
+		free_list(&root_b);
 	}
 	return (0); 
 }
