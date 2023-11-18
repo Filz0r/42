ba332fe795ab24801a901b359a4213ebe7340991
Filz0r <filipefig26@gmail.com>
2023-11-18 12:11:23 +0000

    files that will be used by game engine

---
+++ so_long/src/events.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   events.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 18:08:37 by fparreir          #+#    #+#             */
+/*   Updated: 2023/11/18 11:02:15 by fparreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/so_long.h"
+
+int	on_destroy(t_data *game)
+{
+	mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+	mlx_destroy_display(game->mlx_ptr);
+	free(game->mlx_ptr);
+	return (0);
+}
+
+int	on_keypress(int keysym, t_data *game)
+{
+	printf("pressed: %d\n", keysym);
+	if (keysym == XK_Escape)
+		quit_game(game);
+	return (0);
+}
+
+int	quit_game(t_data *game)
+{
+	mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+	mlx_destroy_display(game->mlx_ptr);
+	free(game->mlx_ptr);
+	free_map(game);
+	exit(1);
+}
+
+void	my_mlx_pixel_put(t_data *game, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = game->img->addr + \
+	(y * game->imgadded new rule to test for segmentation faults in clion->line_length + x * (game->img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
---
+++ so_long/src/game_start.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_start.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 10:10:36 by fparreir          #+#    #+#             */
+/*   Updated: 2023/11/18 11:00:27 by fparreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/so_long.h"
+
+void	game_init(t_data *game)
+{
+	game->mlx_ptr = mlx_init();
+	if (game->mlx_ptr == NULL)
+		return ;
+	game->win_ptr = mlx_new_window(game->mlx_ptr, \
+		(game->map->width * TILE_SIZE), \
+		(game->map->height * TILE_SIZE), "so_long");
+	if (game->win_ptr == NULL)
+		return (free(game->mlx_ptr));
+	mlx_hook(game->win_ptr, KeyRelease, \
+		KeyReleaseMask, on_keypress, game);
+	mlx_hook(game->win_ptr, \
+	DestroyNotify, StructureNotifyMask, \
+	quit_game, game);
+
+	mlx_loop(game->mlx_ptr);
+}
+
+//mlx_hook(game->win_ptr, KeyPress, KeyPressMask, quit_game, game);
+
+/*game->img->img = mlx_xpm_file_to_image(game->mlx_ptr, \
+	WALL_PATH, &game->img->width, &game->img->height);
+
+mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, \
+	game->img->img, 0, 0);
+*/
