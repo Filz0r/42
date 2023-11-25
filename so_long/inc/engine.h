2ce6aa043772c6be0eeda5ea210e1b2b93bb92fc
Filz0r <filipefig26@gmail.com>
2023-11-25 18:17:15 +0000

    fixing some things

--- so_long/inc/engine.h
+++ so_long/inc/engine.h
@@ -6,14 +6,14 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 10:35:15 by fparreir          #+#    #+#             */
-/*   Updated: 2023/11/24 21:59:16 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/11/25 12:25:22 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENGINE_H
 # define ENGINE_H
 
-# define CYCLE		10000000000
+# define CYCLE		1000000000
 # define SIZE		32
 # define UP			1
 # define DOWN		2
@@ -30,6 +30,6 @@
 // Library stuff
 void	*game_init(char **map, const char *name);
 void	*game_cleanup(void *ptr);
-void	*game_run(void *ptr);
+void	game_run(void *ptr);
 
 #endif //ENGINE_H
