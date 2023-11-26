98d8f764e68d4a4ecf7abb0bb148f538ffffae87
Filz0r <filipefig26@gmail.com>
2023-11-26 19:14:52 +0000

    norm errors

--- so_long/src/engine/images.c
+++ so_long/src/engine/images.c
@@ -6,7 +6,7 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 16:44:45 by fparreir          #+#    #+#             */
-/*   Updated: 2023/11/26 16:34:55 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/11/26 18:26:37 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,14 @@ t_img	*create_image(char *path, t_window *win)
 	if (!res)
 		return (NULL);
 	res->img_ptr = mlx_xpm_file_to_image
-		(win->mlx_ptr, path, &res->w, &res->h);
-	if (!res->img_ptr)
+		(win->mlx_ptr, path, &(res->w), &(res->h));
+	if (!(res->img_ptr))
 	{
 		ft_putendl_fd(path, 2);
 		ft_putendl_fd("Error: file could not be read", 2);
 		free(res);
 		return (NULL);
 	}
-
 	res->addr = mlx_get_data_addr
 		(res->img_ptr, &(res->bpp), &(res->line_len), &(res->endian));
 	res->win = win;
@@ -77,23 +76,3 @@ void	load_frames(t_window *w, t_list **lst, char *path, int size)
 	}
 }
 
-// Finds the color of the current pixel we are trying to draw
-unsigned int	get_pixel_img(t_img img, int x, int y)
-{
-	return (*(unsigned int *)(
-		(img.addr + (y * img.line_len) + (x * img.bpp / 8))));
-}
-
-// puts the pixel inside the image object
-void	put_pixel_img(t_img img, int x, int y, int color)
-{
-	char	*dest;
-
-	if (color == (int)0x00000000)
-		return ;
-	if (x >= 0 && y >= 0 && x < img.w && y < img.h)
-	{
-		dest = img.addr + (y * img.line_len + x * (img.bpp / 8));
-		*(unsigned int *)dest = color;
-	}
-}
