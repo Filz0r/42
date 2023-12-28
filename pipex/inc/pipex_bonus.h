35ba3f97dbae7cc0f997969a2597b11322a71f5b
Filz0r <filipefig26@gmail.com>
2023-12-28 14:05:03 +0000

    added the bonus functions to the headerfile

--- pipex/inc/pipex_bonus.h
+++ pipex/inc/pipex_bonus.h
@@ -6,7 +6,7 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 20:00:06 by fparreir          #+#    #+#             */
-/*   Updated: 2023/12/15 18:43:06 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/12/21 19:17:26 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 # define PIPEX_BONUS_H
 
 # include <pipex.h>
+
+int		usage(void);
+t_pipe	handle_heredoc(char *endfile, const char *eof);
+void	fill_heredoc(int fd, const char *eof);
 
 #endif //PIPEX_BONUS_H
