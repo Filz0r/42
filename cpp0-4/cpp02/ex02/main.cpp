ea5f7d0cabdc4189df82ce64a4a4dad29fd0749c
Filz0r <filipefig26@gmail.com>
2024-04-11 16:45:54 +0100

    made it a little cleaner

--- cpp0-4/cpp02/ex02/main.cpp
+++ cpp0-4/cpp02/ex02/main.cpp
@@ -10,12 +10,14 @@ int main() {
 	std::cout << "B:" << b << std::endl;
 	std::cout << "C:" << c << std::endl;
 	std::cout << "D:" << d << std::endl << std::endl;
+
 	std::cout << "increments" << std::endl;
 	std::cout << "A:" << a << std::endl;
 	std::cout << "A:" << ++a << std::endl;
 	std::cout << "A:" << a << std::endl;
 	std::cout << "A:" << a++ << std::endl;
 	std::cout << "A:" << a << std::endl << std::endl;
+
 	std::cout << "operations" << std::endl;
 	std::cout << a + b << std::endl;
 	std::cout << a + c << std::endl;
@@ -26,6 +28,7 @@ int main() {
 	std::cout << a * b << std::endl;
 	std::cout << a * c << std::endl;
 	std::cout << c * b << std::endl << std::endl;
+
 	std::cout << "validations" << std::endl;
 	std::cout << ((a < c) ? "true" : "false") << std::endl;
 	std::cout << ((a > c) ? "true" : "false") << std::endl;
