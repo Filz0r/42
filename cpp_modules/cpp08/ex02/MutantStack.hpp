1169fcf71ca7fee92728b14bdd2453fee00414f2
Filz0r <filipefig26@gmail.com>
2024-05-31 14:19:15 +0100

    added reverse iterators

--- cpp_modules/cpp08/ex02/MutantStack.hpp
+++ cpp_modules/cpp08/ex02/MutantStack.hpp
@@ -9,6 +9,8 @@ class MutantStack : public std::stack<T> {
 	public:
 		typedef typename std::deque<T>::iterator iterator;
 		typedef typename std::deque<T>::const_iterator const_iterator;
+		typedef typename std::deque<T>::reverse_iterator reverse_iterator;
+		typedef typename std::deque<T>::const_reverse_iterator const_reverse_iterator;
 
 		iterator begin() {
 			return this->c.begin();
@@ -24,5 +26,21 @@ class MutantStack : public std::stack<T> {
 
 		const_iterator end() const {
 			return this->c.end();
+		}
+
+		reverse_iterator rbegin() {
+			return this->c.rbegin();
+		}
+
+		reverse_iterator rend() {
+			return this->c.rend();
+		}
+
+		const_reverse_iterator rbegin() const {
+			return this->c.rbegin();
+		}
+
+		const_reverse_iterator rend() const {
+			return this->c.rend();
 		}
 };
